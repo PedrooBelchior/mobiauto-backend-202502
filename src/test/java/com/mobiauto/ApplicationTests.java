1aca6e3363d75ed23fc36a08b47db56117c0a9d6
PedrooBelchior <Always.X26@gmail.com>
2025-03-06 23:54:35 -0300

    feat: Estruturando crud API.

--- src/test/java/com/mobiauto/ApplicationTests.java
+++ src/test/java/com/mobiauto/ApplicationTests.java
@@ -2,12 +2,15 @@ package com.mobiauto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = Application.class) // Explicitamente define a classe de configuração
 @SpringBootTest
-class ApplicationTests {
+public class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        // Teste básico
+    }
 }
